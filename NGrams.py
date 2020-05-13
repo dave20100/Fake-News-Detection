cae3db4f457845a1a062c0f3384f68edcbd8075d
dave20100 <dave201000@gmail.com>
2020-05-13 18:25:30 +0200

    formatted the scores

--- NGrams.py
+++ NGrams.py
@@ -7,12 +7,13 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
 import matplotlib.pyplot as plt
 import copy
 import time
 
+#Set a range of Ngram length that will be tested
+NgramSizeRange = range(1,2)
 
 datasplit = {
     "char": [],
@@ -33,29 +34,21 @@ def basicPreparation(fileName):
     return (file['text'], file['label'])
 
 
-def ngramVectorizationPreparation(articles):
+def dataPreprocessing(articles):
     return
 
-def wordVectorizationPreparation(articles):
+def plotData():
     return
-
 
 #Prepare data
 (text, labels) = basicPreparation("news.csv") 
 
-ngramText = ngramVectorizationPreparation(text)
-
-wordsText = wordVectorizationPreparation(text)
 
 
 # Extract information from data
 
-#Vectorize data
 
-
-start = time.time()
-
-for size in range(1, 4):
+for size in NgramSizeRange:
     extractedData["bagOfWords"]["char"].append({"vectorizer": CountVectorizer(analyzer="char", ngram_range=(size, size))})
     extractedData["tfidf"]["char"].append({"vectorizer": TfidfVectorizer(analyzer="char", ngram_range=(size, size))})
 
@@ -64,6 +57,8 @@ extractedData["bagOfWords"]["word"].append({"vectorizer": CountVectorizer(
 extractedData["tfidf"]["word"].append({"vectorizer": TfidfVectorizer(
     analyzer="word", ngram_range=(1, 1))})
 
+print('{:>18}  {:>18}  {:>18} {:>18} {:>18} {:>18}'.format(
+    "method", "wordType", "size", "classifier", "score", "time"))
 for method in extractedData.keys():
     for wordType in extractedData[method].keys():
         for size in range(len(extractedData[method][wordType])):
@@ -73,32 +68,16 @@ for method in extractedData.keys():
                 "RandomForest": RandomForestClassifier()
             }
             for classifier in extractedData[method][wordType][size]["classificator"].keys():
+                start = time.time()
                 extractedData[method][wordType][size]["features"] = extractedData[method][wordType][size]["vectorizer"].fit_transform(text)
                 X_train, X_test, y_train, y_test = train_test_split(extractedData[method][wordType][size]["features"], labels, test_size=0.60, random_state=42)
                 extractedData[method][wordType][size]["classificator"][classifier].fit(X_train, y_train)
-                print(method, wordType, size+1, classifier, round(extractedData[method][wordType][size]["classificator"][classifier].score(X_test, y_test), 2), "% correct")
+                end = time.time() 
+                print('{:>18}  {:>18}  {:>18} {:>18} {:>18} {:>18}'.format(method, wordType, size+1, classifier,
+                        str(round(extractedData[method][wordType][size]["classificator"][classifier].score(X_test, y_test), 2)) + "% correct ",
+                        str(round(end-start, 2)) + "s czas uczenia"))
 
-end = time.time() / 60
-
-print(end - start)
-# print(extractedData)
-
-# print(len(tfidfVectorizer.get_feature_names()))
-
-#Train data
 
 #Save trained models
 
 #Plot data
-
-x = [xaxis*xaxis for xaxis in range(200)]
-y = [xaxis for xaxis in range(200)]
-
-plt.plot(y, x)
-
-plt.xlabel('x')
-plt.ylabel('y')
-
-plt.title('My first graph!')
-plt.ion()
-plt.savefig('./plots/file.png')

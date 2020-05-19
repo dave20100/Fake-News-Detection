873547dbae5c11f1f565b5cbedde65eef03bf536
dave20100 <dave201000@gmail.com>
2020-05-19 23:41:12 +0200

    Sorted imports and removed not used function

--- NGrams.py
+++ NGrams.py
@@ -1,24 +1,24 @@
-import pandas as pd
-from enum import Enum
-import string
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import HashingVectorizer
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn import svm
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neural_network import MLPClassifier
-import matplotlib.pyplot as plt
 import copy
-import time
-import nltk
 import re
+import string
+import time
+from enum import Enum
+
+import matplotlib.pyplot as plt
+import nltk
+import pandas as pd
 from nltk.corpus import stopwords
+from sklearn import preprocessing, svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import (CountVectorizer,
+                                             HashingVectorizer,
+                                             TfidfVectorizer)
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 
 #Set a range of Ngram length that will be tested
-NgramSizeRange = range(1,3)
+NgramSizeRange = range(1,2)
 
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
@@ -38,22 +38,14 @@ def basicPreparation(fileName):
     file = pd.read_csv(fileName)
     label_encoder = preprocessing.LabelEncoder()
     file['label'] = label_encoder.fit_transform(file['label']) #labelling data FAKE = 0 REAL = 1
-    file = file.applymap(lambda s: s.lower() if type(s) == str else s) #set everything to lowercase
+    file = file.applymap(lambda s: s.lower() if type(s) == str else s) #set every letter to lowercase
     return (file['text'], file['label'])
-
-def removeSpecialCharacters(character):
-    if(character == '.'):
-        return ' '
-    if(character.isalnum() or character == ' '):
-        return character
-    return ''
-
 
 def dataPreprocessing(articles, labels):
     deletionIndexes = []
     for articleIndex in range(len(articles)):
-        articles[articleIndex] = ''.join(w+' ' for w in articles[articleIndex].split(' ') if not w in stop_words and w != '')  # remove special characters
-        articles[articleIndex] = re.sub(r'[^a-zA-Z]+', ' ', articles[articleIndex])
+        articles[articleIndex] = ''.join(w+' ' for w in articles[articleIndex].split(' ') if not w in stop_words and w != '')  # remove all stopwords
+        articles[articleIndex] = re.sub(r'[^a-zA-Z]+', ' ', articles[articleIndex]) #remove all special characters
         articleLength = len(articles[articleIndex])
         if(articleLength == 0 or articleLength < 500 or articleLength > 5000):
             deletionIndexes.append(articleIndex) #save indexes of rows that need to be deleted
@@ -93,14 +85,13 @@ for method in extractedData.keys():
             }
             for classifier in extractedData[method][wordType][size]["classificator"].keys():
                 extractedData[method][wordType][size]["features"] = extractedData[method][wordType][size]["vectorizer"].fit_transform(text)
-                # print(extractedData[method][wordType][size]["vectorizer"].get_feature_names())
                 X_train, X_test, y_train, y_test = train_test_split(extractedData[method][wordType][size]["features"], labels, test_size=0.60, random_state=42)
                 start = time.time()
                 extractedData[method][wordType][size]["classificator"][classifier].fit(X_train, y_train)
                 end = time.time() 
                 print('{:>18}  {:>18}  {:>18} {:>18} {:>18} {:>22}'.format(method, wordType, size+1, classifier,
                         str(round(extractedData[method][wordType][size]["classificator"][classifier].score(X_test, y_test), 2)) + "%",
-                        str(round(end-start, 2)) + "s czas uczenia"))
+                        str(round(end-start, 2)) + "s"))
 
 
 #Save trained models

a58503d60748a37b138ec8f5dd6885696c8481b1
dave20100 <dave201000@gmail.com>
2020-08-16 00:40:16 +0200

    update

--- Application/NGrams.py
+++ Application/NGrams.py
@@ -22,8 +22,7 @@ from datetime import datetime
 
 #Set a range of Ngram length that will be tested
 minNgram = 1
-maxNgram = 8
-
+maxNgram = 6
 NgramSizeRange = range(minNgram, maxNgram)
 
 nltk.download('stopwords')
@@ -50,8 +49,11 @@ def basicPreparation(fileName):
 def dataPreprocessing(articles, labels):
     deletionIndexes = []
     for articleIndex in range(len(articles)):
-        articles[articleIndex] = ''.join(w+' ' for w in articles[articleIndex].split(' ') if not w in stop_words and w != '')  # remove all stopwords
-        articles[articleIndex] = re.sub(r'[^a-zA-Z]+', ' ', articles[articleIndex]) #remove all special characters
+        articles[articleIndex] = ''.join(w+' ' for w in articles[articleIndex].split(
+            ' ') if not w in stop_words and w != '')  # remove all stopwords
+        # remove all special characters
+        articles[articleIndex] = re.sub(
+            r'[^a-zA-Z]+', ' ', articles[articleIndex])
         articleLength = len(articles[articleIndex])
         if(articleLength == 0 or articleLength < 500 or articleLength > 5000):
             deletionIndexes.append(articleIndex) #save indexes of rows that need to be deleted
@@ -59,16 +61,18 @@ def dataPreprocessing(articles, labels):
     labels = labels.drop(deletionIndexes, axis=0)
     return (articles, labels)
 
-def plotData():
-    return
-
 #Prepare data
 (text, labels) = basicPreparation("news.csv") 
 (text, labels) = dataPreprocessing(text, labels)
 
+X_train, X_test, y_train, y_test = train_test_split(
+    text, labels, test_size=0.40, random_state=42)
+
 for size in NgramSizeRange:
-    extractedData["bagOfWords"]["Ngram"].append({"vectorizer": CountVectorizer(analyzer="char", ngram_range=(size, size))})
-    extractedData["tfidf"]["Ngram"].append({"vectorizer": TfidfVectorizer(analyzer="char", ngram_range=(size, size))})
+    extractedData["bagOfWords"]["Ngram"].append(
+        {"vectorizer": CountVectorizer(analyzer="char", ngram_range=(size, size))})
+    extractedData["tfidf"]["Ngram"].append(
+        {"vectorizer": TfidfVectorizer(analyzer="char", ngram_range=(size, size))})
 
 extractedData["bagOfWords"]["Word"].append({"vectorizer": CountVectorizer(
     analyzer="word", ngram_range=(1, 1))})
@@ -78,53 +82,51 @@ extractedData["tfidf"]["Word"].append({"vectorizer": TfidfVectorizer(
 print('{:^20}  {:^20}  {:^20} {:^20} {:^20} {:^20} {:^20}'.format(
     "Vectorization", "Word type", "Size", "Classifier", "Score", "Learn time", "Test time"))
 
-with open('./Wyniki/' + str(datetime.now().strftime("%d%m%Y%H%M%S")) + str(minNgram) + 'to' + str(maxNgram) + '.csv', mode='w', newline='') as score_file:
-    csv_writer = csv.writer(score_file)
-    csv_writer.writerow(["Vectorization", "Word Type", "Size", "Classifier", "Score", "Learn time", "Test time"])
-    for method in extractedData.keys():
-        for wordType in extractedData[method].keys():
-            for size in range(len(extractedData[method][wordType])):
-                extractedData[method][wordType][size]["classificator"] = {
-                    "SVC": svm.SVC(),
-                    "KNN": KNeighborsClassifier(),
-                    "RandomForest": RandomForestClassifier(),
-                    "MLP": MLPClassifier(),
-                    "Naive Bayes": MultinomialNB()
-                }
-                X_train, X_test, y_train, y_test = train_test_split(text, labels, test_size=0.40, random_state=42)
+for method in extractedData.keys():
+    for wordType in extractedData[method].keys():
+        for size in range(len(extractedData[method][wordType])):
+            extractedData[method][wordType][size]["classificator"] = {
+                "KNN": KNeighborsClassifier(),
+                "RandomForest": RandomForestClassifier(),
+                "Naive Bayes": MultinomialNB(),
+                "SVC": svm.SVC(),
+                "MLP": MLPClassifier()
+            }
 
-                extractedData[method][wordType][size]["vectorizer"].fit(X_train)
-                
-                trainingFeatures = extractedData[method][wordType][size]["vectorizer"].transform(X_train)
-                testFeatures = extractedData[method][wordType][size]["vectorizer"].transform(X_test)
+            extractedData[method][wordType][size]["vectorizer"].fit(X_train)
+            
+            trainingFeatures = extractedData[method][wordType][size]["vectorizer"].transform(
+                X_train)
+            testFeatures = extractedData[method][wordType][size]["vectorizer"].transform(
+                X_test)
 
-                scalerTrain = StandardScaler(with_mean=False)
-                scalerTest = StandardScaler(with_mean=False)
-                trainingFeatures = scalerTrain.fit_transform(trainingFeatures)
-                testFeatures = scalerTest.fit_transform(testFeatures)
-                
-                for classifier in extractedData[method][wordType][size]["classificator"].keys():
+            scalerTrain = StandardScaler(with_mean=False)
+            scalerTest = StandardScaler(with_mean=False)
+            trainingFeatures = scalerTrain.fit_transform(trainingFeatures)
+            testFeatures = scalerTest.fit_transform(testFeatures)
 
-                    startLearn = time.time()
-                    extractedData[method][wordType][size]["classificator"][classifier].fit(trainingFeatures, y_train)
-                    endLearn = time.time() 
+            for classifier in extractedData[method][wordType][size]["classificator"].keys():
+                startLearn = time.time()
+                extractedData[method][wordType][size]["classificator"][classifier].fit(
+                    trainingFeatures, y_train)
+                endLearn = time.time()
 
-                    startTest = time.time()
-                    acuraccy = extractedData[method][wordType][size]["classificator"][classifier].score(
-                        testFeatures, y_test)*100
-                    endTest = time.time()
+                startTest = time.time()
+                acuraccy = extractedData[method][wordType][size]["classificator"][classifier].score(
+                    testFeatures, y_test)*100
+                endTest = time.time()
 
+                with open('./Wyniki/' + classifier + '.csv', mode='a', newline='') as score_file:
+                    csv_writer = csv.writer(score_file)
                     csv_writer.writerow([method, wordType, minNgram+size, classifier,
-                                         str(round(acuraccy, 2)) + "%", 
-                                         str(round(endLearn-startLearn, 3)),
-                                         str(round(endTest-startTest, 3))])
-                    print('{:^20}  {:^20}  {:^20} {:^20} {:^20} {:^20} {:^20}'.format(method, wordType, minNgram+size, classifier,
-                            str(round(acuraccy, 2)) + "%",
-                            str(round(endLearn-startLearn, 3)) + "s",
-                            str(round(endTest-startTest, 3)) + "s"))
+                                            str(round(acuraccy, 2)) + "%", 
+                                            str(round(endLearn-startLearn, 3)),
+                                            str(round(endTest-startTest, 3))])
+
+                print('{:^20}  {:^20}  {:^20} {:^20} {:^20} {:^20} {:^20}'.format(method, wordType, minNgram+size, classifier,
+                        str(round(acuraccy, 2)) + "%",
+                        str(round(endLearn-startLearn, 3)) + "s",
+                        str(round(endTest-startTest, 3)) + "s"))
 
                             
 
-
-
-#Plot data
